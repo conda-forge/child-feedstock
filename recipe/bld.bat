54852b9211b81d611987cddcc44319907e534a4c
mcflugen <mcflugen@gmail.com>
2021-03-12 20:10:11 -0700

    fix build scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,15 @@
 setlocal EnableDelayedExpansion
 
 :: Make a build folder and change to it.
-cd src
-mkdir build
-cd build
+mkdir src/build
+cd src/build
 
 :: Configure using the CMakeFiles
 cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
-      %SRC_DIR%
+      %SRC_DIR%/src
 if errorlevel 1 exit 1
 
 :: Build!
